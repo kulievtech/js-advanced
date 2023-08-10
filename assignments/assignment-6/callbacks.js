de235b6cf5c3bc027a3d84648fbc7e8e379ac2fe
Tohir Kuliev <kuliev.tech@gmail.com>
2023-08-10 10:25:16 -0400

    fully finish assignment-6

--- assignments/assignment-6/callbacks.js
+++ assignments/assignment-6/callbacks.js
@@ -17,7 +17,7 @@ function filterOddNumbers(array) {
     return oddNumArray;
 }
 
-console.log(filterOddNumbers([1, 2, 3, 4, 5])); // Output: [1, 3, 5]
+// console.log(filterOddNumbers([1, 2, 3, 4, 5])); // Output: [1, 3, 5]
 
 // Numbers Divisible by Three
 // Write a function named filterDivisibleByThree that takes an array of numbers and returns a new array that contains numbers that are divisible by 3.
@@ -35,7 +35,7 @@ function filterDivisibleByThree(array) {
     return numbersDivByThreeArray;
 }
 
-console.log(filterDivisibleByThree([3, 6, 7, 9, 12])); // Output: [3, 6, 9, 12]
+// console.log(filterDivisibleByThree([3, 6, 7, 9, 12])); // Output: [3, 6, 9, 12]
 
 // Prime Numbers Filter
 // Write a function named filterPrimeNumbers that takes an array of numbers and returns a new array that contains only prime numbers.
@@ -65,7 +65,7 @@ function filterPrimeNumbers(array) {
     return onlyPrimeNumbersArray;
 }
 
-console.log(filterPrimeNumbers([2, 3, 4, 5, 6])); // Output: [2, 3, 5] )
+// console.log(filterPrimeNumbers([2, 3, 4, 5, 6])); // Output: [2, 3, 5] )
 
 // Perfect Squares Filter
 // Write a function named filterPerfectSquares that takes an array of numbers and returns a new array that has perfect squares.
@@ -88,7 +88,7 @@ function filterPerfectSquares(array) {
     return perfectSquaresArray;
 }
 
-console.log(filterPerfectSquares([1, 4, 5, 9, 16])); // Output: [1, 4, 9, 16] );
+// console.log(filterPerfectSquares([1, 4, 5, 9, 16])); // Output: [1, 4, 9, 16] );
 
 // USING CALLBACK NOW !!!
 
@@ -132,10 +132,10 @@ function isPerfectSquare(number) {
 
 const arrayInput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 16, 20];
 
-console.log(filterArray(arrayInput, isOdd));
-console.log(filterArray(arrayInput, isDivisibleByThree));
-console.log(filterArray(arrayInput, isPrimeNumber));
-console.log(filterArray(arrayInput, isPerfectSquare));
+console.log(filterArray(arrayInput, isOdd)); // output: [ 1, 3, 5, 7, 9, 11 ]
+console.log(filterArray(arrayInput, isDivisibleByThree)); // output: [ 3, 6, 9, 12 ]
+console.log(filterArray(arrayInput, isPrimeNumber)); // output: [ 2, 3, 5, 7, 11 ]
+console.log(filterArray(arrayInput, isPerfectSquare)); // output: [ 1, 4, 9, 16 ]
 
 // part 2
 // In this assignment, you will be working on four different problems. Each problem will require you to write a function that processes an array of strings according to specific criteria. Initially, you should solve the problems using your current understanding of JavaScript. After that, analyze your solutions and look for patterns that can be refactored using callbacks and generic functions. Let's dive into the problems!
@@ -160,7 +160,7 @@ function filterPalindromes(array) {
     return onlyPalindromeStringArray;
 }
 
-console.log(filterPalindromes(["racecar", "madam", "apple"])); // Output: ["racecar", "madam"] );
+// console.log(filterPalindromes(["racecar", "madam", "apple"])); // Output: ["racecar", "madam"] );
 
 // Lowercase Strings Only
 // Write a function named filterLowercase that takes an array of mixed lowercase and uppercase lettered words. Return a new array that contains only strings that are entirely lowercase.
@@ -176,9 +176,144 @@ function filterLowercase(array) {
     for (let i = 0; i < array.length; i++) {
         const string = array[i];
 
-        if (isLowerCase(string)) onlyLowerCaseStringArray.push(string);
+        if (isLowerCase(string)) {
+            onlyLowerCaseStringArray.push(string);
+        }
     }
     return onlyLowerCaseStringArray;
 }
 
-console.log(filterLowercase(["apple", "Banana", "cherry"])); // Output: ["apple", "cherry"] );
+// console.log(filterLowercase(["apple", "Banana", "cherry"])); // Output: ["apple", "cherry"] );
+
+// Consonant Lettered Words
+// Write a function named filterConsonantWords that takes an array of strings and returns a new array of strings that have only consonant letters.
+
+function isConsonantWord(word) {
+    const vowels = "aeiou";
+    for (let i = 0; i < word.length; i++) {
+        const eachLetterOfWord = word[i].toLowerCase();
+        if (vowels.includes(eachLetterOfWord)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function filterConsonantWords(array) {
+    let consonantWordArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const string = array[i];
+
+        if (isConsonantWord(string)) {
+            consonantWordArray.push(string);
+        }
+    }
+    return consonantWordArray;
+}
+
+// console.log(filterConsonantWords(["rhythm", "sky", "fly", "apple"])); // Output: ["rhythm", "sky", "fly"] ));
+
+function isOneVowelWord(word) {
+    let countOfVowelWord = 0;
+    const vowels = "aeiou";
+
+    for (let i = 0; i < word.length; i++) {
+        const eachLetterOfWord = word[i].toLowerCase();
+
+        if (vowels.includes(eachLetterOfWord)) {
+            countOfVowelWord++;
+            if (countOfVowelWord > 1) {
+                return false;
+            }
+        }
+    }
+    return countOfVowelWord === 1;
+}
+
+function filterOneVowelWords(array) {
+    let oneVowelWordArray = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const string = array[i];
+
+        if (isOneVowelWord(string)) {
+            oneVowelWordArray.push(string);
+        }
+    }
+
+    return oneVowelWordArray;
+}
+
+// console.log(filterOneVowelWords(["cat", "dog", "apple"])); // Output: ["cat", "dog"]
+
+// USING CALLBACK NOW !!!
+
+function filterArray1(array, callback) {
+    const filteredArray1 = [];
+
+    for (let i = 0; i < array.length; i++) {
+        const string = array[i];
+
+        if (callback(string)) {
+            filteredArray1.push(string);
+        }
+    }
+    return filteredArray1;
+}
+
+function isPalindrome(word) {
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === lowerCaseWord.split("").reverse().join("");
+}
+
+function isLowerCase(string) {
+    const lowerCased = string.toLowerCase();
+    return lowerCased === string;
+}
+
+function isConsonantWord(word) {
+    const vowels = "aeiou";
+    for (let i = 0; i < word.length; i++) {
+        const eachLetterOfWord = word[i].toLowerCase();
+        if (vowels.includes(eachLetterOfWord)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isOneVowelWord(word) {
+    let countOfVowelWord = 0;
+    const vowels = "aeiou";
+
+    for (let i = 0; i < word.length; i++) {
+        const eachLetterOfWord = word[i].toLowerCase();
+
+        if (vowels.includes(eachLetterOfWord)) {
+            countOfVowelWord++;
+            if (countOfVowelWord > 1) {
+                return false;
+            }
+        }
+    }
+    return countOfVowelWord === 1;
+}
+
+const arrayInput1 = [
+    "fly",
+    "Apple",
+    "Banana",
+    "sky",
+    "hi",
+    "Elephant",
+    "Cat",
+    "dog",
+    "Racecar",
+    "madam"
+];
+
+console.log(filterArray1(arrayInput1, isPalindrome)); // output: [ 'Racecar', 'madam' ]
+console.log(filterArray1(arrayInput1, isLowerCase)); // output: [ 'fly', 'sky', 'hi', 'dog', 'madam' ]
+console.log(filterArray1(arrayInput1, isConsonantWord)); // output: [ 'fly', 'sky' ]
+console.log(filterArray1(arrayInput1, isOneVowelWord)); // output: [ 'hi', 'Cat', 'dog' ]
